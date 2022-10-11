b299c6c5652ed3aa99cf12934d6b74a338188263
Matias Fernadez <fernandezalmedamatias@gmail.com>
2022-10-11 20:08:30 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserContainer from "./components/UserContainer/UserContainer";
+
+function App() {
+  return (
+    <div>
+      <UserContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserContainer/UserContainer.jsx
@@ -0,0 +1,18 @@
+import './UserContainer.css'
+
+
+function UserContainer () {
+    return (
+      <section className='principal-section'>
+        <div className='titulo-container'>
+            <h1 >Matias Fernandez Almeda</h1>
+        </div>
+        <div className='parrafo-container'>
+            <p> Frontend Web Debeloper.</p>
+        </div>
+      </section>
+    );
+  }
+  
+  export default UserContainer ;
+  

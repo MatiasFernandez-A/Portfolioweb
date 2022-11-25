6a3aedf4318b72d9a3a5dbc935b232c2e3aab596
Matias Fernadez <fernandezalmedamatias@gmail.com>
2022-11-24 21:54:43 -0300

    EStilos al grid, about, circles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import NavBar from "./components/NavBar/NavBar";
 import MainContainer from "./components/MainContainer/MainContainer";
 import FooterContainer from "./components/FooterContainer/FooterContainer";
+import CirclesContainer from "./components/CirclesContainer/CirclesContainer";
 
 function App() {
   return (
     <div>
+
+      <CirclesContainer/>
+
       <NavBar />
       <MainContainer />
       <FooterContainer />

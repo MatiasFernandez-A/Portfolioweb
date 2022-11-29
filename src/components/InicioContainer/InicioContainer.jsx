93f2d088ab08a31d0e14e4536d7bc9c441dd755a
Matias Fernadez <fernandezalmedamatias@gmail.com>
2022-11-29 20:45:14 -0300

    Agregando animacion de circulos

--- src/components/InicioContainer/InicioContainer.jsx
+++ src/components/InicioContainer/InicioContainer.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import CirclesContainer from '../CirclesContainer/CirclesContainer'
 import './InicioContainer.css'
-
-
 function InicioContainer() {
   return (
     <div className="seccion" id="inicio">
-            <CirclesContainer/>
-            <section className="section info">
-                <h2 className="h2">Matias Fernandez Almeda</h2>
-                <h3 className="h3">Frontend Developer</h3>
+      <section className="section info">
+        <h2 className="h2">Matias Fernandez Almeda</h2>
+        <h3 className="h3">Frontend Developer</h3>
 
-                <ul className="ul">
-                  <li className="li">
-                    <a href="#" className="a" title="Github">Visita mi GitHub</a>
-                  </li>
-                  <li className="li">
-                    <a href="#" className="a" title="LinkedIn">Visita mi Linkedin</a>
-                    </li>
-                </ul>
-            </section>
-        </div>
+        <ul className="ul">
+          <li className="li">
+            <a href="#" className="a" title="Github">Visita mi GitHub</a>
+          </li>
+          <li className="li">
+            <a href="#" className="a" title="LinkedIn">Visita mi Linkedin</a>
+          </li>
+        </ul>
+      </section>
+    </div>
   )
 }
 
